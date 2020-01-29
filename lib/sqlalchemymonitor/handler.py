752708bbf8e9485030923cb9f8439a4798c8cda4
Lucas Limeira <lucasalveslm@gmail.com>
2020-01-29 10:44:31 -0300

    added support to custom handlers by operation (select, update and insert)

--- lib/sqlalchemymonitor/handler.py
+++ lib/sqlalchemymonitor/handler.py
@@ -1,16 +1,20 @@
 import logging
 
 class SqlAchemyLogHandler(logging.Handler):
-    def __init__(self):
+    def __init__(self, default_action, on_select=None, on_insert=None, on_update=None, on_not_recognized=None):
         super().__init__()
         self.setFormatter(logging.Formatter('SLH: %(asctime)s - %(name)s - %(levelname)s - %(message)s'))
         self.setLevel(logging.INFO)
+        self.default_action = default_action
+        self.on_select = on_select
+        self.on_insert = on_insert
+        self.on_update = on_update
+        self.on_not_recognized = on_not_recognized
 
     def emit(self, record):
         message = record.msg
         splitted = message.split(' ', maxsplit=1)
         if len(splitted) == 1:
-            # Single term operation
             return
 
         operation, tail = splitted
@@ -20,20 +24,20 @@ class SqlAchemyLogHandler(logging.Handler):
                 arguments = tail.split('FROM', maxsplit=1)
                 if len(arguments) > 1:
                     table = arguments[1].strip().split(' ', maxsplit=1)[0]
-                    print('%s: %s' % (operation, table))
+                    (self.on_select or self.default_action)(operation, table)
             elif operation == 'INSERT':
                 arguments = tail.split('INTO', maxsplit=1)
                 if len(arguments) > 1:
                     table = arguments[1].strip().split(' ', maxsplit=1)[0]
-                    print('%s: %s' % (operation, table))
+                    (self.on_insert or self.default_action)(operation, table)
             elif operation == 'UPDATE':
                 arguments = tail.split(' ', maxsplit=1)
                 if len(arguments) > 0:
                     table = arguments[0].strip()
-                    print('%s: %s' % (operation, table))
+                    (self.on_update or self.default_action)(operation, table)
             else:
-                # Not recognized operation
-                return
+                if self.on_not_recognized:
+                    self.on_not_recognized(message)
         except:
-            # Not recognized operation
-            return
+            if self.on_not_recognized:
+                self.on_not_recognized(message)
